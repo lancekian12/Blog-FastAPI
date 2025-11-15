7c5488b5c08b23724347fdf30eee7106e9b9e168
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-15 09:33:08 +0800

    create a separate file for schemas

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from schemas import Blog
 
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str
 
 @app.post('/blog')
 def create_blog(blog: Blog):
