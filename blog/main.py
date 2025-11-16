050a6c5c5ed4dd86ea43825d4dfd6fd7fc4c01dd
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-16 11:17:23 +0800

    fix(blog): correct create endpoint to return 201 status code feat(blog): add exception handling for get single blog by ID

--- blog/main.py
+++ blog/main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Response, Depends, status, HTTPException
 from . import schemas, models
 from .database import engine, Sessionlocal
 from sqlalchemy.orm import Session
 
 
-
-
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = Sessionlocal()
@@ -17,7 +16,8 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -25,12 +25,19 @@ def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+
 @app.get('/blog')
-def all(db:Session = Depends(get_db)):
+def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(
+        models.Blog.id == id).first()
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {id} is not available")
     return blog
