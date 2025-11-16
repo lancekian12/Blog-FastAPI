488178662dc856592e8685f04d50dc0a60c24156
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-16 12:23:42 +0800

    feat(blog): add response model for get all blogs and get single blog

--- blog/main.py
+++ blog/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Response, Depends, status, HTTPException
 from . import schemas, models
 from .database import engine, Sessionlocal
 from sqlalchemy.orm import Session
-
+from typing import List
 
 app = FastAPI()
 
@@ -26,18 +26,26 @@ def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowAllBlogs])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBloG)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(
         models.Blog.id == id).first()
 
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with the id {id} is not available")
+                            detail=f"Blog with the id {id} is not found")
     return blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id ==
+                                 id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,15 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBloG(BaseModel):
+    title: str
+    class Config():
+        orm_mode = True
+        
+class ShowAllBlogs(BaseModel):
+    title: str
+    body: str
+    
+    class Config():
+        orm_mode = True
