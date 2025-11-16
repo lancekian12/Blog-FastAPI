d3228c670e913e6b1192796f0f886606a4c847fa
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-16 10:56:32 +0800

    store the blog in the database

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, Sessionlocal
+from sqlalchemy.orm import Session
+
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post('/blog')
-def create_blog(blog: schemas.Blog):
-    return blog
+def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
