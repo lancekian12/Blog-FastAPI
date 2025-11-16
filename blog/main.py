59892daa5a0603bef8748a34948fb96ca8d7f4dd
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-16 12:26:45 +0800

    feat(blog): add PUT endpoint to edit a blog by ID

--- blog/main.py
+++ blog/main.py
@@ -49,3 +49,13 @@ def delete(id: int, db: Session = Depends(get_db)):
                                  id).delete(synchronize_session=False)
     db.commit()
     return 'done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, blog: schemas.Blog, db: Session = Depends(get_db)):
+    updated_blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not updated_blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {id} is not found")
+    updated_blog.update({'title': blog.title, 'body': blog.body})
+    db.commit()
+    return 'updated'
