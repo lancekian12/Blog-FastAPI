f40f8a2b61bcf947306d58e89f53d02d1000751d
Lance Kian Flores <148456275+lancekian12@users.noreply.github.com>
2025-11-21 23:04:30 +0800

    chore: add dependencies and update README and add ,gitignore in the project

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Response, Depends, status, HTTPException
 
-from .schemas import schemas
 from .models import models
 from .database.database import engine, Sessionlocal, get_db
 from .utils.security import Hash
@@ -18,5 +17,3 @@ get_db = get_db
 app.include_router(blog.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
-
-
